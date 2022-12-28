85bcad534a6aaa1d7c7d8fc8cc522e84d7ee733a
said hasnaoui <saidhasnaoui.uce@gmail.com>
2022-12-28 15:32:06 +0100

    Initial commit

---
+++ src/main/java/next/youbooking/yb/YouBookingBackEndApplication.java
@@ -0,0 +1,13 @@
+package next.youbooking.yb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouBookingBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouBookingBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/next/youbooking/yb/YouBookingBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package next.youbooking.yb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouBookingBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
